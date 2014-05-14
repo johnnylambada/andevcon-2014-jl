523c31dc2e0eccb3749f8580c4f54c26445ceeab
John Lombardo <john@lombardos.org>
2014-05-14 11:11:22 -0700

    updates to fragment-logged

---
+++ fragment-logged/src/main/java/andevcon14/FragmentLogged/MenuActivity.java
@@ -0,0 +1,19 @@
+package andevcon14.FragmentLogged;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_layout);
+    }
+
+    public void startStaticFragmentActivity(View view){
+        startActivity(new Intent(this,StaticFragmentActivity.class));
+    }
+}
